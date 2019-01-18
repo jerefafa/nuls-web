22e37e7395cc7c8a7ec21f302fbea688e9b8d14e
Jeremiah Malicdem <jmalicdem45@gmail.com>
2019-01-18 10:11:00 +0800

    fix: reg

--- register-mobile.php
+++ register-mobile.php
@@ -7,12 +7,12 @@ $user = json_decode($_POST["user"]);
 
 if(mysqli_num_rows($conn->query("SELECT * FROM `users` WHERE `username` = '$user->username' OR `id_number` = '$user->id_num'")) == 0 ) {
     if ($conn->query("INSERT INTO `users`(`user_fname`,`user_lname`,`user_mname`,`id_number`,`username`,`password`,`user_type`,`course_id`) VALUES ('$user->fname','$user->lname','$user->mname','$user->id_num','$user->username','" . password_hash($user->password, PASSWORD_DEFAULT) . "','$user->user_type','$user->course_id')")) {
-        echo json_encode($conn->insert_id);
+        echo json_encode(json_encode($conn->insert_id));
     } else {
-        echo json_encode(0);
+        echo json_encode("failed");
     }
 }
 else {
-    echo json_encode(2);
+    echo json_encode("taken");
 }
 
