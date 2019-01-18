e2ec8f1c41e39e259cc755c4b4fb16e6628aefa9
Jeremiah Malicdem <jmalicdem45@gmail.com>
2019-01-18 09:48:01 +0800

    fix: registration

--- register-mobile.php
+++ register-mobile.php
@@ -3,10 +3,10 @@ header("Content-Type:application/json");
 header("Access-Control-Allow-Origin: *");
 
 $conn = new mysqli("nuls-mysqldbserver.mysql.database.azure.com","nuls-user@nuls-mysqldbserver","National1","nuls");
-$user = json_decode($_POST[""]);
+$user = json_decode($_POST["user"]);
 if($conn->query("INSERT INTO `users`(`user_fname`,`user_lname`,`user_mname`,`id_number`,`username`,`password`,`user_type`,`course_id`) VALUES ('$rser->fname','$user->lname','$user->mname','$user->id_num','$user->username','".password_hash($user->password)."','$user->user_type','$user->course_id')")) {
-    echo $conn->insert_id;
+    echo json_encode($conn->insert_id);
 } else {
-    echo 0;
+    echo json_encode(0);
 }
 
