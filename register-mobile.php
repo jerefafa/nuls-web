2cac23fcf84d2ab7dd62969c9e5a5f961ad2141d
Jeremiah Malicdem <jmalicdem45@gmail.com>
2019-01-18 10:03:30 +0800

    fix: reg

--- register-mobile.php
+++ register-mobile.php
@@ -4,9 +4,15 @@ header("Access-Control-Allow-Origin: *");
 
 $conn = new mysqli("nuls-mysqldbserver.mysql.database.azure.com","nuls-user@nuls-mysqldbserver","National1","nuls");
 $user = json_decode($_POST["user"]);
-if($conn->query("INSERT INTO `users`(`user_fname`,`user_lname`,`user_mname`,`id_number`,`username`,`password`,`user_type`,`course_id`) VALUES ('$rser->fname','$user->lname','$user->mname','$user->id_num','$user->username','".password_hash($user->password)."','$user->user_type','$user->course_id')")) {
-    echo json_encode($conn->insert_id);
-} else {
-    echo json_encode(0);
+
+if(mysqli_num_rows($conn->query("SELECT * FROM `users` WHERE `username` = '$user->username' OR `id_number` = '$user->id_num'")) == 0 ) {
+    if ($conn->query("INSERT INTO `users`(`user_fname`,`user_lname`,`user_mname`,`id_number`,`username`,`password`,`user_type`,`course_id`) VALUES ('$user->fname','$user->lname','$user->mname','$user->id_num','$user->username','" . password_hash($user->password, PASSWORD_DEFAULT) . "','$user->user_type','$user->course_id')")) {
+        echo json_encode($conn->insert_id);
+    } else {
+        echo json_encode(0);
+    }
+}
+else {
+    echo json_encode(2);
 }
 
