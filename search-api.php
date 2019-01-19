42e3653dddbc1afff5d5d9da0c1c7c1ac15acc74
Jeremiah Malicdem <jmalicdem45@gmail.com>
2019-01-20 07:53:14 +0800

    fixes

--- search-api.php
+++ search-api.php
@@ -13,7 +13,7 @@ if(isset($_POST["keyword"])){
     $stmt = $conn->query("SELECT DISTINCT `acquisition`.`acquisition_number`,`acquisition`.`title`,`acquisition`.`author`,`acquisition`.`edition`,`acquisition`.`copyright_date` FROM `acquisition` INNER JOIN `keywords`,`subjects` WHERE (`acquisition`.`title` LIKE '%".$keyword."%' OR `acquisition`.`author` LIKE '%".$keyword."%') OR (`keywords`.`keyword` LIKE '%".$keyword."%' AND `keywords`.`acquisition_number` = `acquisition`.`acquisition_number`) OR (`subjects`.`subject_name` LIKE '%".$keyword."%' AND `acquisition`.`subject_id` = `subjects`.`subject_id`) AND (`acquisition`.`date_deleted` IS NULL)");
     $res = array();
     while ($row = $stmt->fetch_object()){
-        if($conn->query(" SELECT `catalog`.`catalog_id` FROM `catalog` WHERE `acquisition_number` = '".$row->acquisition_number."' AND `is_missing` = '0' AND `is_borrowed` = '0' AND `date_deleted` IS NULL") > 0) {
+        if(mysqli_num_rows($conn->query(" SELECT `catalog`.`catalog_id` FROM `catalog` WHERE `acquisition_number` = '".$row->acquisition_number."' AND `is_missing` = '0' AND `is_borrowed` = '0' AND `date_deleted` IS NULL")) > 0) {
             array_push($res, $row);
         }
     }
