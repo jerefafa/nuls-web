4fc0dc84951d3848e24e62ef2bf68507e22c2214
Jeremiah Malicdem <jmalicdem45@gmail.com>
2019-01-20 07:45:49 +0800

    some fixes

--- user-borrow-search.php
+++ user-borrow-search.php
@@ -10,7 +10,7 @@ header("Access-Control-Allow-Origin: *");
 if(isset($_POST["barcode"])) {
     $conn = new mysqli("nuls-mysqldbserver.mysql.database.azure.com","nuls-user@nuls-mysqldbserver","National1","nuls");
     $barcode = $_POST["barcode"];
-    $stmt = $conn->query("SELECT `acquisition`.`acquisition_number`,`acquisition`.`title`,`acquisition`.`author`,`acquisition`.`edition`FROM `acquisition` INNER JOIN `catalog` WHERE `acquisition`.`acquisition_number` = `catalog`.`acquisition_number` AND `catalog`.`barcode` = '" . $barcode . "' AND `catalog`.`date_deleted` IS NULL");
+    $stmt = $conn->query("SELECT `acquisition`.`acquisition_number`,`acquisition`.`title`,`acquisition`.`author`,`acquisition`.`edition`,`acquisition`.`copyright_date` FROM `acquisition` INNER JOIN `catalog` WHERE `acquisition`.`acquisition_number` = `catalog`.`acquisition_number` AND `catalog`.`barcode` = '" . $barcode . "' AND `catalog`.`date_deleted` IS NULL");
     $res = array();
     while ($row = $stmt->fetch_object()) {
         array_push($res, $row);
